c902ac23faa440595129dbdd363d600eace4dee3
angelinabraznikova <fksimka@gmail.com>
2023-01-12 20:40:37 +0300

    Created class Session for session managment

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,14 +10,14 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="dfdfdf", header="dfdfdfd", footer="dffdf"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
 
