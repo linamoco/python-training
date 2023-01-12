27a4788838e558944ade8e6c061269bebf215e29
angelinabraznikova <fksimka@gmail.com>
2023-01-12 20:31:59 +0300

    Created packages

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
@@ -9,13 +9,15 @@ def app(request):
     request.addfinalizer(fixture.destroy)  # удаляем фикустуру с помощью функции инициализации фикстуры
     return fixture
 
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="dfdfdf", header="dfdfdfd", footer="dffdf"))
+    app.logout()
+
 def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
 
 
-def test_add_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="dfdfdf", header="dfdfdfd", footer="dffdf"))
-    app.logout()
+
